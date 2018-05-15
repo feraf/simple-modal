5e79e22b12fed76e31482b08ef95ff90bb6dd025
Fernando de Figueiredo <FerAFigueiredo@outlook.com>
2018-05-14 22:50:58 -0300

    Changed modal setup. Now the classes and attributes starts with sm-

--- js/simple-modal.js
+++ js/simple-modal.js
@@ -1,26 +1,41 @@
-window.onload = function(){
-    let $ = function(param){
-        return document.querySelector(param);
-    }
+var initSimpleModal = function(){
+    let $ = param => document.querySelector(param);
+    let $s = param => document.querySelectorAll(param);
+    
+    let modals = $s("[sm-modal]");
+    modals.forEach(modal => {
+        modal.classList.add("sm-modal");
+        let modalType = modal.getAttribute("sm-modal");
+        if (modalType) modal.classList.add(modalType);
+    });
 
-    console.log("Initializing modal openers click handlers.");
-
-    let openers = document.querySelectorAll("[data-modal-opener]");
+    let openers = $s("[sm-open]");
     openers.forEach(opener => {
         opener.onclick = function(){
-            let target = this.getAttribute("data-modal-opener");
-            console.log(target);
-            if (target){
-                $(target).classList.add("show");
-                let overlay = document.createElement("div");
-                overlay.setAttribute("id", "modal-overlay");
-                overlay.classList.add("overlay");
-                overlay.onclick = function(){
-                    overlay.remove();
-                    $(target).classList.remove("show");
-                }
-                $("body").appendChild(overlay);
-            }
+            let target = this.getAttribute("sm-open");
+            if (!target) return;
+
+            $(target).classList.add("show");
+            createOverlay(target);
         }
-    })
+    });
+
+    let createOverlay = function(targetName){
+        let target = $(targetName);
+        let overlay = document.createElement("div");
+
+        overlay.setAttribute("id", "modal-overlay");
+        overlay.classList.add("sm-overlay");
+
+        let blurElement = target.getAttribute("sm-blur");
+        if (blurElement) $(blurElement).classList.add("sm-blur");
+
+        overlay.onclick = function(){
+            overlay.remove();
+            target.classList.remove("show");
+            if (blurElement) $(blurElement).classList.remove("sm-blur");
+        }
+
+        $("body").appendChild(overlay);
+    }
 }
